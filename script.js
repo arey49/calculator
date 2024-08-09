ca8b77715fe12e0dec236b6b6bc7dea93b51bac9
arey49 <antonioreyatienza@gmail.com>
2024-08-09 13:41:51 +0200

    Add functions to click on numbers
    Add functions to add numbers to the display when they are clicked. Clear button also resets screen to 0. Added effect to button clicks

--- script.js
+++ script.js
@@ -29,9 +29,36 @@ function operate(operand1, operator, operand2) {
     }
 }
 
+function buttonPressed(event) {
+    const buttonClasses = event.target.classList;
+
+    if (buttonClasses.contains("operand-button")) { // classList isn't an array. Can't use .includes()
+        addDigitToDisplay(event.target);
+    }
+    else if (buttonClasses.contains("options-button")) {
+        if (event.target.id == "reset-button") {
+            clearScreen();
+        }
+    }
+
+}
+
+function addDigitToDisplay(button) {
+    if (screen.textContent == "0") {
+        screen.textContent = "";
+    }
+
+    screen.textContent += button.textContent;
+}
+
+function clearScreen() {
+    screen.textContent = "0";
+}
+
 let operand1;
 let operand2;
 let operator;
 
 const screen = document.querySelector(".screen");
-const buttonsDiv = document.querySelector(".buttons-div")
+const buttonsDiv = document.querySelector(".buttons-div");
+buttonsDiv.addEventListener("click", buttonPressed);
